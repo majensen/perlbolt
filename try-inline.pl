60e39bcd9cbdb03a02d340c60b039dcfcaf835af
Mark A. Jensen <maj-git@fortinbras.us>
2019-01-08 22:48:00 -0500

    rm cruft

--- try-inline.pl
+++
@@ -1,8 +0,0 @@
-use lib '.';
-use Bolt;
-  
-my $c = Bolt->connect_("neo4j://127.0.0.1:7687");
-my $pm;
-my $rs = $c->run_query("match (a) return labels(a), a.name",$pm);
-my $r = $rs->fetch_next_;
-1;
